f58d2032473412dbaaa313c4436fc3a0e424ad03
Katsu <keesmark@gmail.com>
2017-06-30 06:16:21 +0000

    added col

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
-  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -19,41 +19,43 @@ class TasksController < ApplicationController
       flash[:success] = 'Task が投稿されました！'
       redirect_to @task
     else
-      flash[:danger] = '失敗です！Task が投稿されていません！！'
+      flash.now[:danger] = '失敗です！Task が投稿されていません！！'
       render :new
     end
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
     if @task.update(task_params)
       flash[:success] = 'Task は更新されました！'
       redirect_to @task
     else
       flash.now[:danger] = 'エラー！Task は更新されませんでした。'
-      rander :edit
+      render :edit
     end
     
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Taskは削除されました!'
     redirect_to tasks_url
   end
   
-  
-
-
-  private
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
+
+    private
+    def set_task
+      @task = Task.find(params[:id])
+    end
+    
+    def task_params
+      params.require(:task).permit(:content, :status)
+    end
 end
+
